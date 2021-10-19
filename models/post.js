216f9ecccc724fc6e26fece97163ae3cfeb9ddbf
Sophie Deskin <sophiedeskin@gmail.com>
2021-10-19 16:52:09 -0700

    updating post model

--- models/post.js
+++ models/post.js
@@ -27,6 +27,11 @@ Post.init(
         key: "id",
       },
     },
+    date_created: {
+    type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+        },
   },
   {
     sequelize,
